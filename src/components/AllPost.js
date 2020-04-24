2784ba7eeee0809a3ed0a2ba5683d70f546f3122
Sophia <sophierithous@gmail.com>
2020-04-24 13:40:08 +0100

    css

--- src/components/AllPost.js
+++ src/components/AllPost.js
@@ -7,7 +7,7 @@ const AllPost = () => {
     <div>
       <h1>All Posts</h1>
       {posts.map(post => (
-        <div key={post.id}>
+        <div className="card" key={post.id}>
           <h2>{post.title}</h2>
           <p>{post.message}</p>
         </div>
